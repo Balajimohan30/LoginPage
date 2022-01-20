652b84761d9f51d497f0214dd880c5a1cced4404
Balajimohan30 <balajismohan30@gmail.com>
2022-01-20 08:11:14 +0530

    datatype

---
+++ src/org/data/Student.java
@@ -0,0 +1,27 @@
+package org.data;
+
+public class Student {
+	public static void main (String []args) {
+		byte age=20 ;
+		System.out.println("age is:"+age);
+		short pin=5555 ;
+		System.out.println("pin is:"+pin);
+		int studId=4563;
+		System.out.println("student ID:"+studId);
+		long phono=987456321;
+		System.out.println("phone no is:"+phono);
+		float salary=85645.215f;
+		System.out.println("salary:"+salary);
+		double stock=8965.234;
+		System.out.println("stock:"+stock);
+		char gender='m';
+		System.out.println("gender is:"+gender);
+		String name= "balaji";
+		System.out.println("name is:"+name);
+		boolean maritalStatus=true;
+		System.out.println("married:"+maritalStatus);
+	
+	
+	}
+
+}
